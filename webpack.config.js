d57552d386017fd7ca804330815035ff3aaaced8
Ricardo Dias <ricardodc@gmail.com>
2017-02-07 04:34:39 -0200

    Finished the template

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
     template: "./index.html",
@@ -12,16 +14,17 @@ module.exports = {
     entry : "./src/index.ts",
     output : {
         filename : "bundle.js",
-        path : "./dist/"
+        path: path.resolve(__dirname, 'dist')
     },
     devtool : "source-map",
     resolve : {
         extensions : [".webpack.config.js","web.js",".ts",".tsx",".js",".jsx",".d.ts"]
     },
     module : {
-        loaders : [
-            {test : /\.ts?$/, loader : "ts-loader"}
-        ]
+        rules: [{
+            test: /\.tsx?$/,
+            loader: 'ts-loader'
+        }]
     },
     plugins : [HtmlWebpackPluginConfig]
 
