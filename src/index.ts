1a3e8f7368dd6bf3f4cd262a79d2fc3a6a6e5c7f
Ricardo Dias <ricardodc@gmail.com>
2017-02-18 17:29:42 -0200

    Using a class to structure initial application

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,38 @@
 import {Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, Mesh, MeshBasicMaterial } from 'three';
 
+class Main{
+    scene:Scene = new Scene();
+    camera:PerspectiveCamera = new PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+    renderer : WebGLRenderer = new WebGLRenderer();
+    geometry : BoxGeometry = new BoxGeometry(1,1,1);
+    material : MeshBasicMaterial = new MeshBasicMaterial({ color : 0x00ff00 });
+    cube : Mesh = new Mesh(this.geometry, this.material);
+    
+    constructor(){
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+    
+        this.scene.add(this.cube);
+        this.camera.position.z = 5;
+        document.body.appendChild(this.renderer.domElement);
+    }
 
-const scene = new Scene();
-const camera = new PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-const renderer : WebGLRenderer = new WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+    render(){
+        requestAnimationFrame(()=>this.render());
 
-const body: HTMLElement = document.getElementsByTagName("body")[0];
-body.appendChild(renderer.domElement);
-//document.body.appendChild(renderer);
+        this.cube.rotation.x += 0.01;
+        this.cube.rotation.y += 0.01;
+        this.renderer.render(this.scene, this.camera); 
 
-const geometry = new BoxGeometry(1,1,1);
-const material = new MeshBasicMaterial({ color : 0x00ff00 });
-const cube = new Mesh(geometry, material);
+    }
 
-scene.add(cube);
-
-camera.position.z = 5;
-
-
-
-const render = function(){
-    requestAnimationFrame(render);
-
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
-
-
-    renderer.render(scene, camera); 
+    start(){
+        this.render();
+    }
 }
 
 
-render();
+const main = new Main();
+main.start();
 
 
 
