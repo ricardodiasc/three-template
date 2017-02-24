a5cf6553cee81e8439a959fa1ffe4f820dacadff
Ricardo Dias <ricardodc@gmail.com>
2017-02-23 22:08:19 -0300

    Still trying to import a model. But with webpack it may be different.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-import {Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, Mesh, MeshBasicMaterial, MeshPhongMaterial, AmbientLight, PointLight, Color } from 'three';
+import {Scene, PerspectiveCamera, WebGLRenderer,
+         BoxGeometry, Mesh, MeshBasicMaterial,
+          MeshPhongMaterial, AmbientLight, PointLight,
+           Color, JSONLoader, SkinnedMesh, MeshFaceMaterials } from 'three';
+import monkey from 'assets/models/monkey.json';
+
 
 class Main{
     scene:Scene = new Scene();
@@ -13,16 +18,39 @@ class Main{
     pointLight : PointLight;
     
     constructor(){
-        this.pointLight = new PointLight();
-        this.pointLight.position.set(100,100,100);
-        this.pointLight.intensity = 0.8;
+        this.configurePointLight();
+
         this.scene.add(this.pointLight);
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.scene.add(new AmbientLight(new Color(0.2,0.2,0.2).getHex()));
-        
-        this.scene.add(this.cube);
-        this.camera.position.z = 5;
+        this.configureCamera();
+
+        //Leaving the cube out for a moment
+        //this.scene.add(this.cube);
+        this.loadModel();
         document.body.appendChild(this.renderer.domElement);
+    }
+
+    loadModel(){
+        var modelLoader = new JSONLoader();
+        modelLoader.load("assets/models/monkey.json",(geometry,materials)=>{
+            var mesh = new SkinnedMesh(geometry, new MeshFaceMaterials(materials));
+            mesh.position.x = 0; mesh.position.y=0;
+            mesh.position.z=0;
+            this.scene.add(mesh);
+        });
+    }
+
+    configurePointLight(){
+        this.pointLight = new PointLight();
+        this.pointLight.position.set(100,100,100);
+        this.pointLight.intensity = 0.8;
+    }
+
+    configureCamera(){
+        this.camera.position.z = 5;
+        this.camera.lookAt(0,0,0);
+        
     }
 
     render(){
