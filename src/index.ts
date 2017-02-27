1a8319075d9a3e8d5759d0515631a1fb026c87a2
Ricardo Dias <ricardodc@gmail.com>
2017-02-27 04:24:19 -0300

    Finnaly implementing import models with webpack and typescript

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import {Scene, PerspectiveCamera, WebGLRenderer,
          BoxGeometry, Mesh, MeshBasicMaterial,
           MeshPhongMaterial, AmbientLight, PointLight,
-           Color, JSONLoader, SkinnedMesh, MeshFaceMaterials } from 'three';
-import monkey from 'assets/models/monkey.json';
+           Color, JSONLoader, SkinnedMesh,ObjectLoader } from 'three';
+import * as  monkey from './assets/models/monkey.json';
 
 
 class Main{
@@ -11,7 +11,7 @@ class Main{
     renderer : WebGLRenderer = new WebGLRenderer();
     geometry : BoxGeometry = new BoxGeometry(1,1,1);
     
-    material : MeshBasicMaterial = new MeshBasicMaterial({ color : 0x00f900 });
+    material : MeshBasicMaterial = new MeshBasicMaterial({ color : 0x00f900 , wireframe: true});
     materialPhong : MeshPhongMaterial = new MeshPhongMaterial({ color : 0x0000ff });
     cube : Mesh = new Mesh(this.geometry, this.materialPhong);
 
@@ -26,24 +26,24 @@ class Main{
         this.configureCamera();
 
         //Leaving the cube out for a moment
-        //this.scene.add(this.cube);
+       // this.scene.add(this.cube);
+        //console.log(monkey);
         this.loadModel();
         document.body.appendChild(this.renderer.domElement);
     }
 
     loadModel(){
         var modelLoader = new JSONLoader();
-        modelLoader.load("assets/models/monkey.json",(geometry,materials)=>{
-            var mesh = new SkinnedMesh(geometry, new MeshFaceMaterials(materials));
-            mesh.position.x = 0; mesh.position.y=0;
-            mesh.position.z=0;
-            this.scene.add(mesh);
-        });
+        console.log(monkey);
+        var model = modelLoader.parse(monkey);
+        var mesh = new Mesh(model.geometry, this.materialPhong);
+        mesh.position.x = 0; mesh.position.y=0;mesh.position.z = 0;
+        this.scene.add(mesh);
     }
 
     configurePointLight(){
         this.pointLight = new PointLight();
-        this.pointLight.position.set(100,100,100);
+        this.pointLight.position.set(5,5,5);
         this.pointLight.intensity = 0.8;
     }
 
@@ -56,8 +56,8 @@ class Main{
     render(){
         requestAnimationFrame(()=>this.render());
 
-        this.cube.rotation.x += 0.01;
-        this.cube.rotation.y += 0.01;
+        //this.cube.rotation.x += 0.01;
+        //this.cube.rotation.y += 0.01;
         this.renderer.render(this.scene, this.camera); 
 
     }
